c4d894a0f92070f39f2a3b2264f119602f33ec9f
SeyoungCho <wnl383@naver.com>
2025-07-10 10:53:20 +0900

    feat: model-viewer 세팅

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import Link from "next/link";
+import Script from "next/script";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <h1>홈페이지</h1>
       <Link href="/new">새 미션</Link>
-    </div>
+      <Script
+        type="module"
+        src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js"
+      ></Script>
+      <div className="model-viewer">
+        <model-viewer
+          src={"/dol.glb"}
+          ios-src={"/dol.usdz"}
+          alt={"model-viewer"}
+          ar
+          ar-modes="webxr scene-viewer quick-look"
+          ar-scale="auto"
+          environment-image="neutral"
+          camera-controls
+        >
+          <button slot="ar-button" id="ar-button">
+            View in AR
+          </button>
+        </model-viewer>
+      </div>
+    </>
   );
 };
 
