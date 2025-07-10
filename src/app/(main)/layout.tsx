71d03ce1a9691275e835549211d190c293de3f37
SeyoungCho <wnl383@naver.com>
2025-07-10 18:42:19 +0900

    feat: 메인레이아웃의 새 미션 등록 버튼 추가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { UserOutlineIcon } from "@vapor-ui/icons";
+import { PlusBoxIcon, UserOutlineIcon } from "@vapor-ui/icons";
 import Navigation from "@/components/home/navigation";
+import Link from "next/link";
+import { Text } from "@vapor-ui/core";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -17,6 +19,15 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="h-[21.16px]" />
       <Navigation />
       {children}
+      <Link
+        href="/new"
+        className="bg-primary h-[85px] flex justify-center items-center rounded-t-[50px] text-white gap-1.5 -mx-5 fixed bottom-0 w-full"
+      >
+        <Text typography="heading4" className="font-semibold text-white">
+          새 미션 등록하기
+        </Text>
+        <PlusBoxIcon size={23} color="white" />
+      </Link>
     </main>
   );
 };
