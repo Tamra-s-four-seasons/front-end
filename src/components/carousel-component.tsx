549ca0c7f4b0e0f47733a3a627492b8a884629e5
sangwoo <dev.sangwoo@gmail.com>
2025-07-11 00:40:32 +0900

    fix: 캐러셀 스와이프 제어 로직 개선

--- src/components/carousel-component.tsx
+++ src/components/carousel-component.tsx
@@ -192,7 +192,7 @@ const CarouselComponent: React.FC = () => {
       </div>
 
       <div className="flex justify-center px-4">
-        <Link href="#" className="w-[76%] max-w-md">
+        <Link href="/missions" className="w-[76%] max-w-md">
           <button className="w-full bg-[#2DDE72] text-white text-m font-bold py-4 px-8 rounded-[22px] hover:bg-green-500 transition-colors duration-300">
             지도 생성하기
           </button>
