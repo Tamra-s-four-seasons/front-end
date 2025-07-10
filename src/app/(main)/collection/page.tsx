fd19fc68caad82e3453bc4f8a099a574a69ace67
SeyoungCho <wnl383@naver.com>
2025-07-10 15:02:40 +0900

    feat: 페이지 컴포넌트 rsc - useQuery - server action 패턴 구성

--- src/app/(main)/collection/page.tsx
+++ src/app/(main)/collection/page.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import { mockRouteData } from "@/constants/mock-data";
+import Test from "@/components/collection/test";
 
-const CollectionPage = () => {
-  return <div>내 컬렉션</div>;
+const prefetchAllRoutes = async (queryClient: QueryClient) => {
+  const getAllRoutes = async () => {
+    console.log("get all routes rsc");
+    return mockRouteData;
+  };
+  await queryClient.prefetchQuery({
+    queryKey: ["routes"],
+    queryFn: getAllRoutes,
+  });
+};
+
+const CollectionPage = async () => {
+  const queryClient = new QueryClient();
+  await prefetchAllRoutes(queryClient);
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <Test />
+    </HydrationBoundary>
+  );
 };
 
 export default CollectionPage;
