e072205b3f76c9b9b95f57e08d0a8e12fd70c105
SeyoungCho <wnl383@naver.com>
2025-07-10 21:42:33 +0900

    feat: 미션 디테일 페이지 퍼블리싱

--- src/components/home/mission-board.tsx
+++ src/components/home/mission-board.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Badge } from "@vapor-ui/core";
+import Link from "next/link";
 import React from "react";
 
 interface MissionBoardProps {
@@ -18,6 +19,7 @@ const MissionBoard = ({ items }: MissionBoardProps) => {
 };
 
 interface MissionBoardItemProps {
+  missionId: string;
   keyword: string;
   description: string;
   routeName: string;
@@ -26,6 +28,7 @@ interface MissionBoardItemProps {
 }
 
 const MissionBoardItem = ({
+  missionId,
   keyword,
   description,
   routeName,
@@ -33,24 +36,29 @@ const MissionBoardItem = ({
   totalCheckpointCnt,
 }: MissionBoardItemProps) => {
   return (
-    <div className="flex p-5 justify-between items-center bg-white rounded-xl">
-      <div className="flex flex-col gap-1.5">
-        <Badge className="bg-secondary text-primary w-fit rounded-full">
-          {keyword}
-        </Badge>
-        <h1 className="text-lg font-bold">{routeName}</h1>
-        <h2 className="text-sm text-muted-foreground">{description}</h2>
-      </div>
-      <div className="flex flex-col rounded-full h-21 aspect-square bg-secondary items-center justify-center">
-        <span className="text-sm text-gray-500 font-medium">달성 스팟</span>
-        <div>
-          <span className="text-2xl font-semibold text-accent">
-            {completedCheckpointCnt}
-          </span>
-          <span className="text-lg text-gray-500"> / {totalCheckpointCnt}</span>
+    <Link href={`/missions/${missionId}`}>
+      <div className="flex p-5 justify-between items-center bg-white rounded-xl">
+        <div className="flex flex-col gap-1.5">
+          <Badge className="bg-secondary text-primary w-fit rounded-full">
+            {keyword}
+          </Badge>
+          <h1 className="text-lg font-bold">{routeName}</h1>
+          <h2 className="text-sm text-muted-foreground">{description}</h2>
+        </div>
+        <div className="flex flex-col rounded-full h-21 aspect-square bg-secondary items-center justify-center">
+          <span className="text-sm text-gray-500 font-medium">달성 스팟</span>
+          <div>
+            <span className="text-2xl font-semibold text-accent">
+              {completedCheckpointCnt}
+            </span>
+            <span className="text-lg text-gray-500">
+              {" "}
+              / {totalCheckpointCnt}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
