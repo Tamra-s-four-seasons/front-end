62ab5583bfbeb2a4a7559ffee283b77d8575563f
SeyoungCho <wnl383@naver.com>
2025-07-10 16:52:55 +0900

    feat: 메인레이아웃 구성

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { NavRoot, NavList, NavItem, NavLink } from "@vapor-ui/core";
+
+import { UserOutlineIcon } from "@vapor-ui/icons";
+import Navigation from "@/components/home/navigation";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <NavRoot size="md" shape="fill" aria-label="Main Navigation">
-        <NavList>
-          <NavItem>
-            <NavLink href="/">미션 현황</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/collection">내 컬렉션</NavLink>
-          </NavItem>
-        </NavList>
-      </NavRoot>
+    <main className="px-5 pt-[50.45px]">
+      <div className="flex items-center justify-between">
+        <div className="bg-gray-200 h-[44.1px] w-[116.99px]" />
+        <UserOutlineIcon size={35} />
+      </div>
+      <div className="h-[22.29px]" />
+      <h1 className="text-foreground text-[26px] leading-12 font-bold h-12">
+        구름님의 OOO
+      </h1>
+      <div className="h-[21.16px]" />
+      <Navigation />
       {children}
-    </div>
+    </main>
   );
 };
 
